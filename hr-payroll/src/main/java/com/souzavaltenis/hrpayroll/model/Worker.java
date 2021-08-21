f1bdbd3e3139dd9350a4e4b6e83390766c1cefb7
Valtenis Souza <valtenis.souza.1@gmail.com>
2021-08-21 20:38:18 -0300

    RestTemplate

---
+++ hr-payroll/src/main/java/com/souzavaltenis/hrpayroll/model/Worker.java
@@ -0,0 +1,18 @@
+package com.souzavaltenis.hrpayroll.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Worker implements Serializable {
+
+    private Long id;
+    private String name;
+    private Double dailyIncome;
+
+}
