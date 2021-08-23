30e7100012f9c45aa07e71b66bc6f51f8e6eca8f
Valtenis Souza <valtenis.souza.1@gmail.com>
2021-08-23 15:23:56 -0300

    User, Role, many to many association

---
+++ hr-user/src/main/java/com/souzavaltenis/hruser/model/Role.java
@@ -0,0 +1,22 @@
+package com.souzavaltenis.hruser.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="tb_role")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String roleName;
+
+}
---
+++ hr-user/src/main/java/com/souzavaltenis/hruser/model/User.java
@@ -0,0 +1,30 @@
+package com.souzavaltenis.hruser.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name="tb_user")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name="tb_user_role", joinColumns = @JoinColumn(name="user_id"), inverseJoinColumns = @JoinColumn(name="role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+}
