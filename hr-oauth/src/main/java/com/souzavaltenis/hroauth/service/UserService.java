d922745f82c42af1fb52a522b7b167cbae079a09
Valtenis Souza <valtenis.souza.1@gmail.com>
2021-08-23 22:31:01 -0300

    Login, JWT token

--- hr-oauth/src/main/java/com/souzavaltenis/hroauth/service/UserService.java
+++ hr-oauth/src/main/java/com/souzavaltenis/hroauth/service/UserService.java
@@ -2,18 +2,34 @@ package com.souzavaltenis.hroauth.service;
 
 import com.souzavaltenis.hroauth.feignclient.UserFeignClient;
 import com.souzavaltenis.hroauth.model.User;
+import feign.FeignException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     @Autowired
     private UserFeignClient userFeignClient;
 
-    public User findByEmail(String email){
+    public User findByEmail(String email) {
         ResponseEntity<User> userResponseEntity = userFeignClient.findByEmail(email);
         return userResponseEntity.getBody();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = null;
+        try {
+            ResponseEntity<User> userResponseEntity = userFeignClient.findByEmail(username);
+            user = userResponseEntity.getBody();
+        } catch (FeignException e) {
+            throw new UsernameNotFoundException("Email not found");
+        }
+        return user;
     }
 }
